5a76900ff2a0f2bfe7d7957970904b5669be3e7a
Mrinank Gaur <mrinank2484@gmail.com>
2025-08-06 21:01:57 +0530

    minor changes to nav bar

---
+++ app/(dashboard)/_components/invite-button.tsx
@@ -0,0 +1,28 @@
+import { Plus,X } from "lucide-react";
+import { OrganizationProfile } from "@clerk/nextjs";
+
+
+import{
+    Dialog,
+    DialogContent,
+    DialogTrigger,
+    DialogClose
+} from "@/components/ui/dialog"
+
+import { Button } from "@/components/ui/button";
+
+export const InviteButton = () => {
+    return(
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">
+                    <Plus className="h-4 w-4 mr-2"/>
+                    Invite Members
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="p-0 w-[880px] bg-transparent border-none">
+                <OrganizationProfile/>
+            </DialogContent>
+        </Dialog>
+    );
+};
