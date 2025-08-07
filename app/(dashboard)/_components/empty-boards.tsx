6216a00564daf45d77078a40bdb8860a2784a009
Mrinank Gaur <mrinank2484@gmail.com>
2025-08-07 17:21:34 +0530

    feat: adding board

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,19 +1,18 @@
 "use client";
-import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 export const EmptyBoards = () => {
     const {organization} = useOrganization();
-    const create = useMutation(api.board.create);
-    
+    const {mutate, pending} = useApiMutation(api.board.create);
 
     const onClick = () => {
         if(!organization) return;
         
-        create({
+        mutate({
             orgId: organization.id,
             title: "Untitled"
         });
@@ -34,7 +33,7 @@ export const EmptyBoards = () => {
                 Start by creating a board for your organization
             </p>
             <div className="mt-6">
-                <Button onClick={onClick} size="lg">
+                <Button disabled={pending} onClick={onClick} size="lg">
                     Create Board
                 </Button>
             </div>
