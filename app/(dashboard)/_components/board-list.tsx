089321753e385bccbb83edd8f64dbc56b2b8e2e8
Mrinank Gaur <mrinank2484@gmail.com>
2025-08-08 15:45:13 +0530

    completed board cards

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -24,6 +24,24 @@ export const BoardList = ({
 
     const data = useQuery(api.boards.get, {orgId}); // TODO: change to api call
 
+    if(data===undefined){
+      return(
+       <div>
+          <h2 className="text-3xl">
+            {query.favorites? "Favorite Board": "Team Board"}
+          </h2>
+          <div className="grid grid-c1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+            <NewBoardButton orgId={orgId} disabled/>
+            <BoardCard.Skeleton/>
+            <BoardCard.Skeleton/>
+            <BoardCard.Skeleton/>
+            <BoardCard.Skeleton/>
+
+          </div>
+        </div>
+      );
+    };
+
     if(!data?.length && query.search){
         return <EmptySearch/>
     }
