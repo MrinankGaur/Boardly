ceb48cb48b08ade25ed17aecc55d3d97155858f5
Mrinank Gaur <mrinank2484@gmail.com>
2025-08-10 10:57:49 +0530

    added fav functionality

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -9,7 +9,9 @@ import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Actions } from "@/components/actions/actions";
 import { MoreHorizontal } from "lucide-react";
-
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 interface BoardCardProps {
     id: string;
     title: string;
@@ -36,6 +38,25 @@ export const BoardCard = ({
     const { userId } = useAuth();
     const authorLabel = userId === authorId ? "You" : authorName;
     const createdAtLabel = formatDistanceToNow(createdAt, {addSuffix: true});
+    const {
+        mutate: onFavorite,
+        pending: pendingFavorite,
+    }  = useApiMutation(api.board.favorite);
+    const {
+        mutate: onUnfavorite,
+        pending: pendigUnfavorite
+    }  = useApiMutation(api.board.unfavorite);
+
+    const toggleFavorite = () => {
+        if(isFavorite){
+            onUnfavorite({ id })
+                .catch(()=> toast.error("Failed to Uunfavorite"));
+        }else{
+            onFavorite({ id,orgId })
+                .catch(() => toast.error("Failed to favorite"));
+        }
+    };
+
 
     return (
         <Link href={`/board/${id}`}>
@@ -68,8 +89,8 @@ export const BoardCard = ({
                     title = {title}
                     authorLabel = {authorLabel}
                     createdAtLabel = {createdAtLabel}
-                    onClick = {()=>{}}
-                    disabled = {false}
+                    onClick = {toggleFavorite}
+                    disabled = {pendigUnfavorite || pendingFavorite}
                 />
             </div>
         </Link>
--- convex/board.ts
+++ convex/board.ts
@@ -51,7 +51,22 @@ export const remove = mutation({
         if(!identity){
             throw new Error("Unauthorized");
         }
-        // TODO: later check to delete favorite relation as well
+        
+        const userId = identity.subject;
+
+        const existingFavorite = await ctx.db
+            .query("useFavorites")
+            .withIndex("by_user_board",(q)=>
+                    q
+                        .eq("userId",userId)
+                        .eq("boardId",args.id)
+                )
+                .unique();
+
+        if(existingFavorite){
+            await ctx.db.delete(existingFavorite._id);
+        }
+
         await ctx.db.delete(args.id);
     },
 });
@@ -80,6 +95,90 @@ export const update = mutation({
         const board = await ctx.db.patch(args.id, {
             title: args.title,
         });
+        return board;
+    },
+});
+
+export const favorite = mutation({
+    args: {
+        id: v.id("boards"), 
+        orgId: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error("Unauthorized");
+        }
+
+        const board = await ctx.db.get(args.id);
+
+        if(!board){
+            throw new Error("Board not found");
+        }
+
+        const userId = identity.subject;
+
+        const existingFavorite = await ctx.db
+        .query("useFavorites")
+        .withIndex("by_user_board_org", (q)=>
+            q   
+                .eq("userId",userId)
+                .eq("boardId",board._id)
+                .eq("orgId",args.orgId)
+        )
+        .unique();
+
+        if(existingFavorite){
+            throw new Error("Board already favorited");
+        }
+
+        await ctx.db.insert("useFavorites",{
+            userId,
+            boardId: board._id,
+            orgId: args.orgId,
+        });
+
+        return board;
+    },
+});
+
+
+export const unfavorite = mutation({
+    args: {
+        id: v.id("boards"), 
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error("Unauthorized");
+        }
+
+        const board = await ctx.db.get(args.id);
+
+        if(!board){
+            throw new Error("Board not found");
+        }
+
+        const userId = identity.subject;
+
+        const existingFavorite = await ctx.db
+        .query("useFavorites")
+        .withIndex("by_user_board", (q)=>
+            q   
+                .eq("userId",userId)
+                .eq("boardId",board._id) 
+                // TODO: check if orgId needed
+        )
+        .unique();
+
+        if(!existingFavorite){
+            throw new Error("Favorited board not found");
+        }
+
+        await ctx.db.delete(existingFavorite._id);
+
         return board;
     },
 });
