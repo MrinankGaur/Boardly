41bb3ca0cb99546b5dffcd0406293c00ad9c43dc
Mrinank Gaur <mrinank2484@gmail.com>
2025-08-11 14:05:15 +0530

    created room info component

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,14 +1,114 @@
-export const Info = () => {
+"use client";
+import { Id } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Hint } from "@/app/(dashboard)/_components/hint";
+import { useRenameModal } from "@/store/use-rename-modal";
+import { Actions } from "@/components/actions/actions";
+import { Menu } from "lucide-react";
+interface InfoProps{
+    boardId: string;
+}
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+});
+
+const TabSeparator = () => {
+    return (
+        <div
+            className= "text-neutral-300 px-1.5 text-lg"
+        >
+            |
+        </div>
+    )
+}
+
+
+export const Info = ({
+    boardId,
+}: InfoProps) => {
+
+    const { onOpen } = useRenameModal();
+    
+    const data = useQuery(api.board.get, {
+        id: boardId as Id<"boards">
+    });
+
+    if(!data){
+        return <InfoSkeleton/>;
+    }
+
     return (
         <div
             className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md"
         >
-            TODO: Information about the board
+            <Hint
+                label="Go to boards"
+                side="bottom"
+                sideOffset={10}
+            >
+                <Button asChild variant="board" className="px-2">
+                <Link href="/">
+                    <Image
+                  src="/logo.svg"
+                  alt="Boardly Logo"
+                  height={40}
+                  width={40}
+                  className="" 
+                />
+                <span className={cn(
+                    "font-semibold text-xl ml-2 text-black",
+                    font.className,
+                )}>
+                    Boardly
+                </span>
+                </Link>
+            </Button>
+            </Hint>
+            <TabSeparator/>
+            <Hint
+                label="Edit title"
+                side="bottom"
+                sideOffset={10}
+            >
+                <Button
+                    variant="board"
+                    className="text-base font-normal px-2"
+                    onClick={()=>onOpen(data._id,data.title)}
+                >
+                    {data.title}
+                </Button>
+            </Hint>
+            <TabSeparator/>
+            <Actions
+              id = {data._id}
+              title = {data.title}
+              side="bottom"
+              sideOffset={10}  
+            >
+                <div>
+                    <Hint
+                        label="Main menu" side = "bottom" sideOffset={10}
+                    >
+                        <Button size="icon" variant="board">
+                            <Menu />
+                        </Button>
+                    </Hint>
+                </div>
+
+            </Actions>
         </div>
     );
 };
 
-Info.Skelton = function InfoSkeleton(){
+export const InfoSkeleton = ()=>{
     
     return(
         <div
